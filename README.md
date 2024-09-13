2d719113a44f97b12dda554f71fe898a65be675b
andrespi97 <andreslage97@gmail.com>
2024-09-13 13:41:36 +0200

    saludador

---
+++ README.md
@@ -0,0 +1,11 @@
+# Saludator
+
+## Descripcion
+
+Aplicación web que te saluda.
+
+- [ ] crear la estructura html
+
+- [ ] aplicar estilos con css
+
+- [ ] dotar de comportamiento con js
---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("miboton").addEventListener("click", saludar);
+function saludar() {
+  if (document.getElementById("frase").innerHTML == "Saludos amigo") {
+    document.getElementById("frase").innerHTML = "...";
+  } else {
+    document.getElementById("frase").innerHTML = "Saludos amigo";
+  }
+}
